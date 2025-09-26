a0248bf57bdd130879859a62a9296156837d0fdf
Aprendiz <200134777+Fernanda3131@users.noreply.github.com>
2025-09-26 08:18:06 -0500

    Buscar usuarios react_Terminar

---
+++ buscador_de_usuarios/src/componentes/Card.jsx
@@ -0,0 +1,12 @@
+export default function Card(){
+
+    return(
+        <div>
+            <img className="w-16 h-16 rounded-full mx-auto" src="" alt="" />
+            <h3 className="text-center font-bold mt-2">Ana Gomes</h3>
+            <p className="text-center text-sm text-gray-600 mt-2">Desarrolladora frontend </p>
+            <p className=" text-center text-xs mt-1 italic">Leer, escribir, correr</p>
+            <p className=" text-center text-xs mt-1 italic">anag@demo.com</p>
+        </div>
+    )
+}
